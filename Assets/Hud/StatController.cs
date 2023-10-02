2a29880c45125696d413eda72a4d350ad8d09a41
aidan-ek <planetaidan@gmail.com>
2023-10-01 20:40:07 -0400

    smooth oxygen bar

--- Assets/Hud/StatController.cs
+++ Assets/Hud/StatController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Threading;
@@ -8,9 +9,9 @@ public class StatController : MonoBehaviour
     private const int OXYGEN_MAX = 120;
     private const int HUNGER_MAX = 180;
     
-    public int oxygen = OXYGEN_MAX;
-    public int hunger = HUNGER_MAX;
-    public int netOxygen = -5;
+    private int oxygen = OXYGEN_MAX;
+    private int hunger = HUNGER_MAX;
+    public int netOxygen = -2;
 
     private GameObject oxygenBarFill;
 
@@ -22,15 +23,21 @@ public class StatController : MonoBehaviour
     private float timer = 0;
     void Update()
     {
-        // runs code inside the if statement once every second
+        Debug.Log(oxygen);
+        Debug.Log(timer);
+        // changes the rate we decrement the oxygen based on the netoxygen
         timer += Time.deltaTime;
-        if (timer >= 1) {
-            timer -= 1;
-            oxygen += netOxygen;
+        if (timer >= 1f / Math.Abs(netOxygen)) {
+            timer -= 1f / Math.Abs(netOxygen);
+            oxygen += netOxygen / Math.Abs(netOxygen);
         }
 
         // updates oxygen bar display
         oxygenBarFill.gameObject.transform.localScale = new Vector3(oxygen*3.57f/OXYGEN_MAX, 0.58f, 1); // changes the scale based on % full
         oxygenBarFill.gameObject.transform.position = new Vector3(-1.8f*(OXYGEN_MAX - oxygen)/OXYGEN_MAX, -4.17f, 0); // shifts the position by half the scale to match
+    }
+
+    public void AddNetOxygen(int net) {
+        netOxygen += net;
     }
 }
